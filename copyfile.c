2cc8d935ce8a1a7b0039264490c0261e1c367d9a
kwj1399 <kwj1399@gmail.com>
2016-09-05 23:37:20 +0800

    add file

--- copyfile.c
+++ copyfile.c
@@ -1,43 +1,83 @@
 #include <stdio.h>
 #include <stdlib.h>
-int main(int argc,char *argv[])
+/* * * * * * * * * * * * * * * * * * *
+ * CopyFile: 此函数用于复制指定的文件到指定的位置,
+ *        功能算是Windows下的 copy, 或linux下的 cp.
+ * 这里的实现用了 fread() 和 fwrite(), 二进制流的形式读写
+ * 返回值: 非负数: 复制的字节数.
+ *         小于0的数: 请检查文件名是否正确输入, 是否合法.
+ * * * * * * * * * * * * * * * * * * */
+long CopyFile(const char* file_1, const char * file_2)
 {
-    FILE *fRead,*fWrite;
-    char tmp;
-    long charcount=0;
-    if(argc!=3)
+    FILE *pfRead = fopen(file_1, "rb");// pfRead用作复制源.
+    FILE *pfWrite = fopen(file_2, "wb");// pfWrite对应复制出来的新文件.
+    if (NULL == pfRead || NULL == pfWrite)
     {
-      printf("need two files!\n");
-      exit(1);
+        fclose(pfRead);
+        fclose(pfWrite);
+        return -1;
+    }
+    long bytesCount = 0;//统计复制的字节数. long最大可以表示不超过2GB的文件
+
+    /* 因为C没有byte类型，所以这里用char替代
+     * 貌似对大多数机器来说，char都是 单字节
+     */
+    int arrLen = 1024;   //这个是缓存数组的元素大小
+    char bufArr[arrLen]; //这个是 "缓存", 缓存的字节数是 elementSize * arrLen.
+    int copiedLen;      //这个变量用来记录fread函数每一次真正读取的元素数
+    int elementSize = sizeof(bufArr[0]);
+    do
+    {
+        copiedLen = 0;
+
+        copiedLen = fread(bufArr, elementSize, arrLen, pfRead);
+        fwrite(bufArr, elementSize, copiedLen, pfWrite);
+
+        bytesCount += copiedLen * elementSize;
+
+    } while(copiedLen == arrLen);
+
+    //关闭流.
+    fclose(pfRead);
+    fclose(pfWrite);
+
+    return bytesCount;
+}
+
+/* 此程序从命令行参数 获取文件名(路径):
+ *      C:\>application_name file1_name file2_name
+ * Example：
+ *      C:\>cfilecopy.exe e:\abc.txt f:\abc2.txt
+ * abc.txt必须存在，abc2.txt可以不存在，会自动创建文件.
+ * 如果abc2.txt已存在，会被覆盖掉.
+ *   (友情提示:注意保存重要的文件, 别被"盖"了!)
+ * */
+int main(int argc, char *argv[])
+{
+    if(argc == 3)
+    {
+
+        char * f1 = argv[1];
+        char * f2 = argv[2];
+
+        printf("Copy File 1: %s\n To File 2: %s\n", f1, f2);
+        puts("Copying......");
+
+        long bytesCount = CopyFile(f1, f2);
+
+        if (bytesCount < 0)
+        {
+            puts("Fail to copy.");
+        }
+        else
+        {
+            printf("  %ld bytes wrote into %s.\n", bytesCount, f2);
+        }
     }
     else
     {
-        if((fRead=fopen(argv[1],"r"))==NULL)
-           {
-               printf("File1 can not open!\n");
-               exit(1);
-           }
-
-        else if((fWrite=fopen(argv[2],"w"))==NULL)
-            {
-               printf("File2 can not open!\n");
-               exit(1);
-            }
-        else
-            {
-                printf("Copy %s to %s\n",argv[1],argv[2]);
-                puts("Copying....");
-                while((tmp=getc(fRead))!=EOF)
-                {
-                    putc(tmp,fWrite);
-                    charcount++;
-                }
-
-            }
+        puts("parameters error");
     }
-    fclose(fWrite);
-    fclose(fRead);
-    printf("total char :%ld\n",charcount);
+    //getchar();
     return 0;
-
 }
